2691398c67d76ede2b0107a1f9e6b184f1e3a74d
zabron2-aron <93010572+zabron2-aron@users.noreply.github.com>
2021-12-08 14:16:49 +0300

    Just rwa arrangements of code

--- A_zSDMS_Javascript.js
+++ A_zSDMS_Javascript.js
@@ -1,28 +1,18 @@
 
 $(document).ready(function () {
- $(".fa-bars").click(function(){
-   $(".left_sidebar").css("width","25%").css("height","50%").$(".left_sidemenu_list").css("top","15%");
- 
-  });
-
-  $(".fa-user-circle").click(function(){
-  $(".welcome_areatext").css("margin-top","10%"); 
-  $("#footer-home").css("height","0%");
-  
-  });
-
-  $(".fa-laptop-house").click(function(){  
-    $(".welcome_areatext").css("margin-top","-40vh");
-    $("#footer-home").css("height","0%");
-   
-  
-  });
-$(".fa-sms").click(function(){
-  $("#footer-home").css("height","fit-content");
- /* $(".left_sidebar").css("height","fit-content");*/
-  
+$(".fa-bars").click(function(){
 });
-
+$(".fa-user-circle").click(function(){
+$(".welcome_areatext").css("margin-top","10%"); 
+$("#footer-home").css("height","0%");
+});
+$(".fa-laptop-house").click(function(){  
+$(".welcome_areatext").css("margin-top","-40vh");
+$("#footer-home").css("height","0%");
+});
+$(".fa-sms").click(function(){
+$("#footer-home").css("height","fit-content");
+});
 });
 
 
