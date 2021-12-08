71e18b0d840b1fe7ab4574f1d4fa5389aaee3c5e
zabron2-aron <93010572+zabron2-aron@users.noreply.github.com>
2021-12-08 14:41:58 +0300

    Margin-top set to 40%

--- A_zSDMS_Javascript.js
+++ A_zSDMS_Javascript.js
@@ -3,7 +3,7 @@ $(document).ready(function () {
 $(".fa-bars").click(function(){
 });
 $(".fa-user-circle").click(function(){
-$(".welcome_areatext").css("margin-top","20%"); 
+$(".welcome_areatext").css("margin-top","40%"); 
 $("#footer-home").css("height","0%");
 });
 $(".fa-laptop-house").click(function(){  
